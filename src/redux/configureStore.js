53be9ad6d541e22ae4ea3e03023c46f43e61e765
nahnahAJ <nahnahahjuwah@gmail.com>
2022-11-08 13:55:26 +0000

    Combine reducers into a root reducer

---
+++ src/redux/configureStore.js
@@ -0,0 +1,13 @@
+// Importing the necessary methods from Redux Toolkit.
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import addReducer from './books/books';
+import statusReducer from './categories/categories';
+
+// Combine both reducers into a root reducer by using configureStore function.
+const rootReducer = combineReducers({
+  books: addReducer,
+  categories: statusReducer,
+});
+
+const store = configureStore(rootReducer);
+export default store;
