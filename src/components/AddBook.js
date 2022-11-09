0effb3a574ff100418b1f684548cae6cf81ec4a9
nahnahAJ <nahnahahjuwah@gmail.com>
2022-11-09 11:56:30 +0000

    Update input states by Onchange listener

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -1,47 +1,49 @@
-import React from 'react';
+import { React, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { addBook } from '../redux/books/books';
 
 // add functionality for "Add new" button click:
 
 const AddBook = () => {
+  const [author, setAuthor] = useState('');
+  const [title, setTitle] = useState('');
+
+  const onChangeAuthor = (event) => {
+    setAuthor(event.target.value);
+  };
+
+  const onChangeTitle = (event) => {
+    setTitle(event.target.value);
+  };
+
   const dispatch = useDispatch();
 
-  const createdBook = () => {
+  const createdBook = (e) => {
+    e.preventDefault();
     // Take the inputs from the state and  and add them to an object.
-    const title = document.getElementById('title');
-    const author = document.getElementById('author');
-
     // Set the data inputs in the local React state (set title and author).
     // generate a unique id
     // dispatch a corresponding action
-    dispatch(
-      addBook({ id: Date.now(), title: title.value, author: author.value }),
-    );
+    dispatch(addBook({ id: Date.now(), title, author }));
   };
 
   return (
-    <form className="form-container">
+    <form className="form-container" onSubmit={createdBook}>
       <input
-        id="title"
         type="text"
         className="input-text"
         placeholder="Book title"
+        value={title}
+        onChange={onChangeTitle}
       />
       <input
-        id="author"
         type="text"
         className="input-text"
         placeholder="Book author"
+        value={author}
+        onChange={onChangeAuthor}
       />
-      <button
-        type="submit"
-        className="input-submit"
-        onClick={(e) => {
-          e.preventDefault();
-          createdBook();
-        }}
-      >
+      <button type="submit" className="input-submit">
         Add Book
       </button>
     </form>
